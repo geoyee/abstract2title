003ce7ceb6148dc39ed2a5ff0e2f7a1cdd47e63d
geoyee <geoyee@yeah.net>
2022-06-12 23:31:53 +0800

    feat(frontend): init ui

---
+++ frontend/src/components/IMenu/index.js
@@ -0,0 +1,40 @@
+import { 
+  HomeOutlined, 
+  SettingOutlined, 
+  QuestionCircleOutlined, 
+  GithubOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+import React from 'react';
+import './index.css'
+
+function getItem(label, key, icon) {
+  return {
+    label,
+    key,
+    icon,
+  };
+}
+
+const items = [
+  getItem('首页', 'home', <HomeOutlined />),
+  getItem('设置', 'setting', <SettingOutlined />),
+  getItem('使用说明', 'readme', <QuestionCircleOutlined />),
+  getItem('项目链接', 'github', <GithubOutlined />),
+];  // submenu keys of first level
+
+const IMenu = () => {
+  return (
+    <div>
+      <div className='logo' />
+      <h1 className='font'>论文标题生成系统</h1>
+      <Menu 
+        className='ui' 
+        theme='dark' 
+        mode='horizontal' 
+        defaultSelectedKeys={['M1']} 
+        items={items} />
+    </div>
+  );
+};
+
+export default IMenu;
---
+++ frontend/src/components/ITool/index.js
@@ -0,0 +1,45 @@
+import { 
+    CodeSandboxOutlined,
+    DiffOutlined, 
+    CopyOutlined,
+    DeleteOutlined,
+    BoldOutlined,
+    FontSizeOutlined,
+    FontColorsOutlined} from '@ant-design/icons';
+import { Menu } from 'antd';
+import React from 'react';
+import './index.css'
+
+function getItem(label, key, icon) {
+    return {
+        label,
+        key,
+        icon,
+    };
+}
+
+const items = [
+    getItem('标题推理', 'infer', <CodeSandboxOutlined />),
+    getItem('粘贴摘要', 'paste', <DiffOutlined />),
+    getItem('清除摘要', 'delet', <DeleteOutlined />),
+    getItem('复制标题', 'copy', <CopyOutlined />),
+    getItem('字体加粗', 'bold', <BoldOutlined />),
+    getItem('调整大小', 'size', <FontSizeOutlined />),
+    getItem('调整颜色', 'color', <FontColorsOutlined />),
+];  // submenu keys of first level
+
+const ITool = () => {
+    return (
+        <Menu
+            mode='inline'
+            defaultSelectedKeys={['1']}
+            style={{
+                height: '100%',
+                borderRight: 0,
+            }}
+            items={items}
+        />
+    );
+};
+
+export default ITool;
